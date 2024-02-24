a4890f012a6212aa6bdb2b06f2008895b2564b52
Kawamatsuuu <160127578+XyrelTZY@users.noreply.github.com>
2024-02-24 15:18:31 +0800

    Update MagicBall.js

--- MagicBall.js
+++ MagicBall.js
@@ -1,15 +1,30 @@
 const options = [
-    "Nigga",
-    "Alien",
-    "Assuming",
-    "GigaNigga",
-    "Bayot"
+    "It is certain",
+  "Without a doubt",
+  "You may rely on it",
+  "Yes definitely",
+  "It is decidedly so",
+  "As I see it, yes",
+  "Most likely",
+  "Yes",
+  "Outlook good",
+  "Signs point to yes",
+  "Reply hazy try again",
+  "Better not tell you now",
+  "Ask again later",
+  "Cannot predict now",
+  "Concentrate and ask again",
+  "Do not count on it",
+  "Outlook not so good",
+  "My sources say no",
+  "Very doubtful",
+  "My reply is no"
   ];
   const magicBall = document.getElementById("mb_container");
   const response = document.getElementById("response");
   
   function myFunction() {
-    const index = Math.floor(Math.random() * 5);
+    const index = Math.floor(Math.random() * 20);
     const message = options[index];
     magicBall.className = "magicBall_container animate";
     setTimeout(() => {
@@ -26,4 +41,3 @@ const options = [
       document.getElementById("response").style.fontWeight = "600";
     }
   }
-  
